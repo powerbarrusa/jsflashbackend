49870ffe6258c681cd36a893f5c908d0f37d95bc
Barr Covrigaru <barr@covrigaru.com>
2019-01-30 13:58:36 -0700

    trying again

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,16 @@
 
 module.exports = {
 
-  production: {
+  development: {
     client: 'pg',
     connection: 'postgres://localhost/jsflash'
+    },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://qrgjmabjbsyuvf:e82d83cdf99f5d8c0298c1215804acc87e9c23b4af1b95dea337b308340caba0@ec2-23-23-184-76.compute-1.amazonaws.com:5432/de1aj23q79dlov'
     }
   }
+}
+
