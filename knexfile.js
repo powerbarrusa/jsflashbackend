6d5c985a61e7260975d5816b0e9041329a0db42b
Barr Covrigaru <barr@covrigaru.com>
2019-01-30 15:07:26 -0700

    trying again

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://qrgjmabjbsyuvf:e82d83cdf99f5d8c0298c1215804acc87e9c23b4af1b95dea337b308340caba0@ec2-23-23-184-76.compute-1.amazonaws.com:5432/de1aj23q79dlov'
+    connection: process.env.DATABASE_URL
     }
   }
 
