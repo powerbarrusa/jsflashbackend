502734f0802f7075922677dc5209fb7c65cdc3a3
Barr Covrigaru <barr@covrigaru.com>
2019-01-30 14:27:24 -0700

    trying again

--- knexfile.js
+++ knexfile.js
@@ -9,9 +9,8 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'postgres://qrgjmabjbsyuvf:e82d83cdf99f5d8c0298c1215804acc87e9c23b4af1b95dea337b308340caba0@ec2-23-23-184-76.compute-1.amazonaws.com:5432/de1aj23q79dlov'
+    connection: 'postgres://qrgjmabjbsyuvf:e82d83cdf99f5d8c0298c1215804acc87e9c23b4af1b95dea337b308340caba0@ec2-23-23-184-76.compute-1.amazonaws.com:5432/de1aj23q79dlov'
     }
   }
-}
+
 
